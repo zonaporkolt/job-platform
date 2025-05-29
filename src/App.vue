6e7f4b25b441b03ea002568cb9204ec230d6d6dd
zonaporkolt <takaacsm@gmail.com>
2025-05-29 14:24:00 +0200

    feat: add shadcn-vue and implement Button component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
-    <h1>Welcome to the Job Portal</h1>
+    <div class="flex flex-col items-center justify-center gap-y-3 h-screen bg-gray-100">
+      <h1>Welcome to the Job Portal!</h1>
+      <Button>Click me</Button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
