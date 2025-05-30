c33227b38fae2fd1f53081f4789e7292daf6f426
zonaporkolt <takaacsm@gmail.com>
2025-05-30 10:25:00 +0200

    feat: update LandingPage to include LoginWidget

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
+import LoginWidget from '@/components/LoginWidget.vue'
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center gap-y-3 h-full">
-    <h1>Welcome to the Job Portal!</h1>
-    <Button>Click me</Button>
+    <h1 class="text-3xl font-bold text-center">
+      Welcome to the Job Portal!
+    </h1>
+    <LoginWidget />
   </div>
 </template>
