f15e46335649ad5e7a5d6bfe23c6c6720d5a81da
zonaporkolt <takaacsm@gmail.com>
2025-05-30 09:16:58 +0200

    chore: update app logo and title

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@
     id="app"
     class="min-h-screen flex flex-col bg-gray-50 text-gray-900"
   >
-    <header class="p-4 bg-white shadow-md flex justify-between items-center">
-      <h1 class="text-lg md:text-2xl font-bold">
+    <header class="p-2 md:p-4 bg-white shadow-md flex justify-between items-center">
+      <h1 class="text-lg md:text-2xl font-bold flex items-center gap-2">
+        <img src="/logo.svg" alt="Logo" class="h-10 w-10 md:h-16 md:w-16">
         Job Portal
       </h1>
       <nav>
