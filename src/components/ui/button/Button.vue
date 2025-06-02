5916abae40d2875ac92e45bb80a7fdf80d892259
zonaporkolt <takaacsm@gmail.com>
2025-06-02 08:44:43 +0200

    feat: add Sidebar component from shadcn-vue with various subcomponents

--- src/components/ui/button/Button.vue
+++ src/components/ui/button/Button.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-import type { PrimitiveProps } from 'reka-ui'
 import type { HTMLAttributes } from 'vue'
-import type { ButtonVariants } from '.'
-import { Primitive } from 'reka-ui'
+import { Primitive, type PrimitiveProps } from 'reka-ui'
 import { cn } from '@/lib/utils'
-import { buttonVariants } from '.'
+import { type ButtonVariants, buttonVariants } from '.'
 
 interface Props extends PrimitiveProps {
   variant?: ButtonVariants['variant']
