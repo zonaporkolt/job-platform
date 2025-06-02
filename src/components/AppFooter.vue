02507d47b20611f827bc323cf1f6d38444e5ff06
zonaporkolt <takaacsm@gmail.com>
2025-06-02 08:41:06 +0200

    feat: add AppHeader and AppFooter components

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="p-4 bg-gray-100 text-center text-sm text-gray-500">
+    &copy; {{ new Date().getFullYear() }} Job Portal. All rights reserved.
+  </footer>
+</template>
