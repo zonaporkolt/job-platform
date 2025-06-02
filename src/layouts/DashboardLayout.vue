d3cf5bc76f64fb40e41958d741af46c5275acd30
zonaporkolt <takaacsm@gmail.com>
2025-06-02 08:39:28 +0200

    feat: add DashboardLayout and DefaultLayout components

---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import AppSidebar from '@/components/AppSidebar.vue'
+</script>
+
+<template>
+  <div class="min-h-screen flex bg-gray-100">
+    <AppSidebar class="w-64 bg-white shadow-md" />
+
+    <div class="flex-1 flex flex-col">
+      <main class="flex-1 p-6 overflow-y-auto">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import AppFooter from '@/components/AppFooter.vue'
+import AppHeader from '@/components/AppHeader.vue'
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col bg-gray-50 text-gray-900">
+    <AppHeader />
+
+    <main class="flex-1 p-4 md:p-6 lg:p-8">
+      <slot />
+    </main>
+
+    <AppFooter />
+  </div>
+</template>
