6cb26fefc280fdd77c68de8797be951f2faf779e
zonaporkolt <takaacsm@gmail.com>
2025-06-02 10:33:11 +0200

    feat: add job listing and delete functionality to JobSearch page

--- src/pages/JobSearch.vue
+++ src/pages/JobSearch.vue
@@ -1,3 +1,43 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { useCurrentUser } from '@/composables/useCurrentUser'
+import { useJobs } from '@/composables/useJobs'
+
+const { jobs, deleteJob } = useJobs()
+const { isEmployer } = useCurrentUser()
+
+function handleDelete(id: string) {
+  deleteJob(id)
+}
+</script>
+
 <template>
-  Job Search
+  <div v-if="jobs.length" class="space-y-4">
+    <div
+      v-for="job in jobs"
+      :key="job.id"
+      class="border rounded-lg p-4 flex justify-between items-center bg-white"
+    >
+      <div>
+        <div class="font-bold text-lg">
+          {{ job.title }}
+        </div>
+        <div class="text-sm text-gray-600">
+          {{ job.description }}
+        </div>
+        <div class="text-xs text-gray-500 mt-1">
+          Tags: <span v-for="tag in job.tags" :key="tag" class="mr-1">#{{ tag }}</span>
+        </div>
+        <div class="text-xs text-gray-500 mt-1">
+          Salary: {{ job.salary.amount }} {{ job.salary.currency }}
+        </div>
+      </div>
+      <Button v-if="isEmployer" variant="destructive" @click="handleDelete(job.id)">
+        Delete
+      </Button>
+    </div>
+  </div>
+  <div v-else class="text-gray-500 text-center">
+    No jobs posted yet.
+  </div>
 </template>
